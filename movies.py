9a5902715208f4eea6be9d4e27ca6fcde5bcee78
unknown <101wngus@likelion.org>
2020-08-04 10:48:41 +0900

    commit

--- movies.py
+++ movies.py
@@ -1,5 +1,4 @@
 import requests
-# html 파싱하는데 도움을 준다.
 from bs4 import BeautifulSoup
 import csv 
 
@@ -8,7 +7,6 @@ URL = 'https://movie.naver.com/movie/running/current.nhn'
 response = requests.get(URL)
 
 soup = BeautifulSoup( response.text , 'html.parser')
-# new_section = soup.select('div[id=wrap] > div[id=container] > div[id=content] > div[id=main_pack] > .news.mynews.section._prs_nws > ul > li  ')
 mlist = soup.select("div.lst_wrap dt.tit a")
 
 title = []
@@ -27,10 +25,3 @@ for m in mlist:
             fieldnames = ["title","code"]
             csvwriter = csv.DictWriter(csvfile,fieldnames)
             csvwriter.writerow(new_data)
-
-# news = new_section.select('dl > dt > a')
-#  for news in new_section:
-
-
-#      new = news.select_one(' dl > dt > a')
-# #     print(new['title'], new['href'])
