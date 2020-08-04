24993f5966c1ed4af37471d41d7157520d7e8421
unknown <101wngus@likelion.org>
2020-08-04 10:50:23 +0900

    commit

--- movies.py
+++ movies.py
@@ -9,13 +9,7 @@ response = requests.get(URL)
 soup = BeautifulSoup( response.text , 'html.parser')
 mlist = soup.select("div.lst_wrap dt.tit a")
 
-title = []
-code = []
-
 for m in mlist:
-    print(m.text)
-    print(m['href'][28:])
-
     new_data = {
             'title' : m.text,
             'code' : m['href'][28:]
